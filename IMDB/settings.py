4b240483540b151fbc9b494342760ccd7ade8a8e
umairyousaf142 <umairyousaf142@gmail.com>
2025-06-21 03:18:58 +0500

    setting file updated

--- IMDB/settings.py
+++ IMDB/settings.py
@@ -51,6 +51,12 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
+
 ROOT_URLCONF = 'IMDB.urls'
 
 TEMPLATES = [
